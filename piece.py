7767ac9612d7e54f2be9f0ce1e4955cb87024809
Lars Janssen <lars.e.janssen@gmail.com>
2024-11-09 12:22:11 +0100

    some more changes UwU

--- piece.py
+++ piece.py
@@ -12,6 +12,7 @@ class Direction(Enum):
 # @return Possible Moves, isWinningMove
 ###
 def getMoves(board: Board, player1: bool) -> tuple[list[Board], bool]:
+    """Returns all possible moves for a player, given a specifiv board. Also specifies if a move would be a winning one"""
     # TODO: Implement
     # Lars
     return [], False
@@ -22,12 +23,31 @@ def getMoves(board: Board, player1: bool) -> tuple[list[Board], bool]:
 # Player1 moves in positive direction (down)
 # Player2 moves in negative direction (up)
 def getMovesForPosition(board: Board, x: int, y: int) -> list[Board]:
+    """Returns the possible Moves for a single piece on a board"""
     possibleMoves: list[Board] = list()
-    pieceToMove: EPiece = EPiece(board.data[x][y])
-    # if pieceToMove == EPiece.DEFAULT_P1:
+    pieceToMove: EPiece = board.data[x][y]
+    directionsToCheck: list[Direction] = list()
+
+    # TODO: Move all this crap into the getDiagonalContent() function. Its is clutter that is ugly and i hate it.
+
+    # Create a list with all the diagonal directions we need to check for a given piece
+    if pieceToMove == EPiece.EMPTY:
+        print("Cannot check moves for an empty spot")
+        return possibleMoves
+    if pieceToMove == EPiece.DEFAULT_P1 or pieceToMove.value > 2: # Check for these Directions if it's a player 1 piece or a dame
+        directionsToCheck.append(Direction.Down_Left)
+        directionsToCheck.append(Direction.Down_Right)
+    if pieceToMove == EPiece.DEFAULT_P2 or pieceToMove.value > 2: # Check for these Directions if it's a player 2 piece or a dame
+        directionsToCheck.append(Direction.Up_Left)
+        directionsToCheck.append(Direction.Up_Right)
+
+    # Check those diagonals
+    diaToCheck: dict[Direction, list[EPiece]] = getDiagonalContent(board, directionsToCheck, x, y)
+
     return possibleMoves
 
-def CustomDiagonals(board: Board, directions: list[Direction], startX: int, startY: int) -> dict[Direction, list[EPiece]]:
+def getDiagonalContent(board: Board, directions: list[Direction], startX: int, startY: int) -> dict[Direction, list[EPiece]]:
+    """Returns a dict with a direction as a key and the contents of the to the direction appropriate diagonal line"""
     movesForDir: dict[Direction, list[EPiece]] = {}
     for dir in directions:
         movesForDir[dir] = contentOfDiagonals(board, dir, startX, startY)
@@ -35,6 +55,7 @@ def CustomDiagonals(board: Board, directions: list[Direction], startX: int, star
 
 
 def contentOfDiagonals(board: Board, direction: Direction, startX: int, startY: int) -> list[EPiece]:
+    """Returns a list of pieces that represent the contents of a diagonal from a given start point"""
     contentInDir: list[EPiece] = list()
     xSearchModifyer: int = 1
     ySearchModifyer: int = 1
