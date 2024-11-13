d34d671305f5feeabb3d8693122b141cab0b32bb
Elias Wernicke <e.w.1111111119@gmail.com>
2024-11-13 11:09:47 +0100

    add piece test

--- test_piece.py
+++ test_piece.py
@@ -2,7 +2,7 @@ from typing import List
 import copy
 
 from const import Board, EPiece
-from piece import contentOfDiagonals, Direction
+from piece import Direction, contentOfDiagonals, getDiagonalContent 
 from util import formatBoard
 
 def test_contentOfDiagonals():
@@ -35,3 +35,31 @@ def test_contentOfDiagonals():
     assert result_2 == [EPiece.DEFAULT_P2, EPiece.DEFAULT_P1]
     assert result_3 == [EPiece.DEFAULT_P1, EPiece.DEFAULT_P2]
     assert result_4 == [EPiece.DAME_P1, EPiece.DAME_P2, EPiece.DEFAULT_P1]
+
+def test_getDiagonalContent():
+    testBoard = Board([[1, 1, 1, 1, 1, 1],
+                       [1, 1, 1, 1, 1, 1],
+                       [1, 1, 1, 1, 1, 1],
+                       [1, 1, 1, 1, 1, 1],
+                       [1, 1, 1, 1, 1, 1],
+                       [1, 1, 1, 1, 1, 1]])
+    
+    result = getDiagonalContent(testBoard, [Direction.Down_Right], 0, 0)
+
+    # assert
+    assert result == {Direction.Down_Right: [EPiece.DEFAULT_P1, EPiece.DEFAULT_P1, EPiece.DEFAULT_P1, EPiece.DEFAULT_P1, EPiece.DEFAULT_P1]}
+
+    testBoard = Board([[1, 1, 1, 1, 1, 1],
+                       [1, 1, 1, 1, 4, 1],
+                       [1, 2, 1, 3, 1, 1],
+                       [1, 1, 1, 1, 1, 1],
+                       [1, 1, 1, 1, 1, 1],
+                       [2, 1, 1, 1, 2, 1]])
+    
+    result = getDiagonalContent(testBoard, [Direction.Down_Right, Direction.Up_Left, Direction.Down_Left, Direction.Up_Right], 2, 3)
+
+    # assert
+    assert result == {Direction.Down_Right: [EPiece.DEFAULT_P1, EPiece.DEFAULT_P2],
+                      Direction.Up_Left: [EPiece.DEFAULT_P2, EPiece.DEFAULT_P1],
+                      Direction.Down_Left: [EPiece.DEFAULT_P1, EPiece.DEFAULT_P2],
+                      Direction.Up_Right: [EPiece.DAME_P1, EPiece.DAME_P2, EPiece.DEFAULT_P1]}
