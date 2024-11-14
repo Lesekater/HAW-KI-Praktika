02a98fe6cc7b3405e6fdad286a146e889032f58a
Elias Wernicke <e.w.1111111119@gmail.com>
2024-11-14 02:20:25 +0100

    fix error and add game experiments

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import copy
+import sys
 from const import Board, EPiece
 from typing import List, Tuple
 from enum import Enum
@@ -6,24 +8,68 @@ from heuristics import calculateHeuristic, heurisitcTypes
 from piece import getMoves
 from util import formatBoard
 
-def main(openList: List[Board] = [], 
+# testBoard1 = Board.fromIntList([
+#     [1, 0, 1, 0, 1, 0, 1, 0],
+#     [0, 1, 0, 1, 0, 1, 0, 1],
+#     [0, 0, 0, 0, 0, 0, 0, 0],
+#     [0, 0, 0, 0, 0, 0, 0, 0],
+#     [0, 0, 0, 0, 0, 0, 0, 0],
+#     [0, 0, 0, 0, 0, 0, 0, 0],
+#     [2, 0, 2, 0, 2, 0, 2, 0],
+#     [0, 2, 0, 2, 0, 2, 0, 2]])
+
+# testBoard1 = Board.fromIntList([
+#     [1, 0, 0, 0],
+#     [0, 0, 0, 0],
+#     [0, 0, 0, 0],
+#     [0, 0, 0, 2]])
+
+testBoard1 = Board.fromIntList([
+    [1, 1, 0, 0],
+    [0, 0, 0, 0],
+    [2, 0, 2, 2]])
+
+# testBoard1 = Board.fromIntList([
+#     [1, 0, 1, 0, 1],
+#     [0, 1, 0, 1, 0],
+#     [0, 0, 0, 0, 0],
+#     [0, 0, 0, 0, 0],
+#     [2, 0, 2, 0, 2],
+#     [0, 2, 0, 2, 0]])
+
+def main(openList: List[Board] = [testBoard1],
          closedList: List[Board] = [], 
-         usedHeuristic: heurisitcTypes = heurisitcTypes.CountOfPieces
+         usedHeuristic: heurisitcTypes = heurisitcTypes.CountOfPiecesAtEndOfBoard
          ) -> Tuple[bool, List[Board], List[Board]]:
     foundGoal = False
+    initalBoard = copy.copy(openList[0])
+
+    # increase recursion limit
+    # print(print(sys.getrecursionlimit()))
+    sys.setrecursionlimit(2000)
 
     while not foundGoal and len(openList) > 0:
+        # i += 1
         nodeToExpand = openList.pop()
-        (foundGoal, winningBoard) = makeMove(nodeToExpand, openList, closedList)
+        (foundGoal, winningBoard) = makeMove(nodeToExpand, openList, closedList, usedHeuristic)
+
+        print("Latest board:")
+        print("position in tree: " + str(nodeToExpand.g))
+        print("size ol: " + str(len(openList)) + " size cl: " + str(len(closedList)))
+        print(formatBoard(nodeToExpand))
 
     if foundGoal:
+        print("\n")
         print("Goal found!")
         print(formatBoard(winningBoard))
         print("Total moves: " + str(winningBoard.g))
         print ("Full Game: ")
+        print("---END---")
         while winningBoard.parent is not None and winningBoard.g != 0.0:
             print(formatBoard(winningBoard))
             winningBoard = winningBoard.parent
+        print(formatBoard(initalBoard))
+        print("---START---")
     else:
         print("Goal not found!")
 
@@ -31,7 +77,8 @@ def main(openList: List[Board] = [],
 
 def makeMove(nodeToExpand: Board, 
              openList: List[Board], 
-             closedList: List[Board]
+             closedList: List[Board],
+             usedHeuristic: heurisitcTypes = heurisitcTypes.CountOfPieces
              ) -> Tuple[bool, Board]:
     (possibleMoves, isWinningMove) = getMoves(
         board=nodeToExpand, player1=nodeToExpand.player1
@@ -51,7 +98,7 @@ def makeMove(nodeToExpand: Board,
         move.g = nodeToExpand.g + 1
 
         # calculate f
-        heuristic = calculateHeuristic(move)
+        heuristic = calculateHeuristic(move, usedHeuristic)
         move.f = move.g + heuristic
 
         # check open list
@@ -72,3 +119,6 @@ def makeMove(nodeToExpand: Board,
 
     # did not find goal
     return False, None   
+
+if __name__ == "__main__":
+    main()
